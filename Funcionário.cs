588101f823b7558735e1bd996a1737a70a6a6eaa
taissaog <110946885+taissaog@users.noreply.github.com>
2022-09-09 20:53:32 -0300

    Add files via upload

---
+++ Funcionário.cs
@@ -0,0 +1,45 @@
+namespace POO{
+    public class Funcionario{
+
+        public string Nome;
+        public double Salario;
+
+        private int tipo;
+//-----------------------------
+        public string GetNome(){
+            return Nome;
+        }
+        public void SetNome(string Nome){
+            this.Nome = Nome;
+        }
+
+        public double GetSalario(){
+            return Salario;
+        }
+        public void SetSalario(double Salario){
+            this.Salario = Salario;
+        }
+
+        public double GetTipo(){
+            return tipo;
+        }
+        public void SetSalario(int tipo){
+            this.tipo = tipo;
+        }
+        plubic Funcionario(string nome, double Salario, int tipo){
+            this.nome = "";
+            this.salario = salario;
+            this.tipo = tipo;
+        }
+
+        plubic Funcionario(string nome, double Salario, int tipo){
+            this.nome = salario;
+            this.salario = salario;
+            this.tipo = tipo;
+        }
+
+        public double GetBonificacao
+//-----------------------------
+        
+    }
+}
